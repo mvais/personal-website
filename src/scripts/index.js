386e06b68fa6a9d3249fecb92bc0eddfae621bdc
Vais Mahendran <vais.mahendran@gmail.com>
2020-05-03 18:42:25 -0400

    Added a filter for articles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,34 @@
-import list from './modules/list'
+import articles from './modules/articles'
 
-console.log(`Hello ${list[0]}`)
+if (document.querySelector('#articles')) {
+  const articleList = document.querySelector('#articles');
+
+  for (let article of articles) {
+    const li = document.createElement('li');
+
+    li.innerHTML = `(${article.date}) ${article.name}`;
+
+    articleList.appendChild(li);
+  }
+}
+
+if (document.querySelector('#article-filter')) {
+  const input = document.querySelector('#article-filter');
+
+  input.addEventListener('input', function (event) {
+    const articleList = document.querySelector('#articles');
+    const filteredArticles = articles.filter(article => {
+      if (article.name.toLowerCase().includes(input.value)) return true
+    });
+
+    articleList.innerHTML = '';
+
+    for (let article of filteredArticles) {
+      const li = document.createElement('li');
+
+      li.innerHTML = `(${article.date}) ${article.name}`;
+
+      articleList.appendChild(li);
+    }
+  })
+}
---
+++ src/scripts/modules/articles.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    "name": "Investigating the MJHL, OJHL, BCHL",
+    "date": "05-01-2020"
+  },
+  {
+    "name": "Beginner to Game Theory",
+    "date": "05-04-2020"
+  }
+]
