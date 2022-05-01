3fdf9cd2666dcbf17194883d00f9aa19ce8b14d9
Vais Mahendran <vais.mahendran@gmail.com>
2022-04-30 22:13:30 -0400

    basic starting page
    Need to fill out with proper content and pages

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -2,4 +2,11 @@ module.exports = {
   content: ['_site/**/*.html'],
   safelist: [],
   plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto-slab': ['Roboto Slab', 'sans-serif']
+      },
+    },
+  },
 }
